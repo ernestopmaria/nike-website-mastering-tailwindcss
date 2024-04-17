2c789c2148fb89c007b90db315c7e757d849d6cf
ernestopmaria <ernestomaria93@gmail.com>
2024-04-17 06:21:47 -0500

    sections and nav components

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # React + Vite
-54min
+1:17min
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from'./Hero'
+import PopularProducts from './PopularProducts'
+import SuperQuality from './SuperQuality'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import CustomerReviews from './CustomerReviews'
+import Subscribe from './Subscribe'
+import Footer from './Footer'
+
+
+export {
+  Hero,
+  SuperQuality,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer
+}
