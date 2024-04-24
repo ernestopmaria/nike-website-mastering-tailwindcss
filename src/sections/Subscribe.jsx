7d0191b733aa413c552c50584a31b648e8268cbe
ernestopmaria <ernestomaria93@gmail.com>
2024-04-24 06:08:01 -0500

    last update footer

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -9,10 +9,10 @@ const Subscribe = () => {
       <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">Sign Up from
         <span className="text-coral-red"> Updates</span> & Newsletter
       </h3>
-      <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col">
+      <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
         <input type="text" placeholder="subcribe@nike.com" 
         className="input"/>
-        <div className="flex max-sm:justify-end items-center max-sm:w-full  max-sm:mt-5">
+        <div className="flex max-sm:justify-end items-center max-sm:w-full  ">
           <Button fullWidth label="Sign Up" textColor="text-slate-gray"/>
           
         </div>
